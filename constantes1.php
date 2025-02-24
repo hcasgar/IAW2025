e74ec36498cb7e7f63db7901c1a709e2aac7d66d
Hugo Castro <hcasgar1511@gmail.com>
2025-02-24 14:01:00 +0100

    constantes saludo

---
+++ constantes1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Pagina</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+	<?php 
+		define ("SALUDO", "Bienvenido al IES Poligono SUR");
+        echo SALUDO; 
+	?>
+
+</body>
+</html>
